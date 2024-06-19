969b3277a383f25fcd9a27bb821aa7dbfcf3d943
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-06-19 12:47:22 +0200

    Update: Enviroments variables and README.md file updated.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ export default function MapComponent() {
     const position = { lat: 38.565533703998085, lng: -0.091235431018248 };
     return (
         <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY}>
-            <Map style={{ width: "100%", height: "100%" }} defaultCenter={position} defaultZoom={15} mapId={import.meta.env.VITE_MAP_ID}>
+            <Map style={{ width: "100%", height: "100%" }} defaultCenter={position} defaultZoom={15} mapId={import.meta.env.VITE_GOOGLE_MAP_ID}>
                 <AdvancedMarker position={position}></AdvancedMarker>
             </Map>
         </APIProvider>
